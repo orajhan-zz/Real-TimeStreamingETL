9f88343e0dcaec12ca84327fdac9622457c20082
jhan <35212672+orajhan@users.noreply.github.com>
2020-05-12 22:01:00 +0800

    Create docker_run.sh

---
+++ Source/docker_run.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+CONNECT_CONFIG_ID=ocid1.connectharness.oc1.uk-london-1.amaaaaaawiclygaagarqnaynpvnqgxiebsqo4x3h3hq4amsesmr7h2t3fqpa
+
+CONFIG_STORAGE_TOPIC=$CONNECT_CONFIG_ID-config
+OFFSET_STORAGE_TOPIC=$CONNECT_CONFIG_ID-offset
+STATUS_STORAGE_TOPIC=$CONNECT_CONFIG_ID-status
+
+docker build -t kafkasourceconnecttest .
+
+docker run -it --rm --name sourcetest -p 8083:8083 \
+-e GROUP_ID=demo-group \
+-e BOOTSTRAP_SERVERS=cell-1.streaming.uk-london-1.oci.oraclecloud.com:9092 \
+-e CONFIG_STORAGE_TOPIC=$CONFIG_STORAGE_TOPIC \
+-e OFFSET_STORAGE_TOPIC=$OFFSET_STORAGE_TOPIC \
+-e STATUS_STORAGE_TOPIC=$STATUS_STORAGE_TOPIC \
+-v "$PWD"/connect-distributed.properties:/kafka/config.orig/connect-distributed.properties:z \
+kafkasourceconnecttest

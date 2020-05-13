93e570c18dffaf3e546c736d66fbb74a4c8e3a07
jhan <35212672+orajhan@users.noreply.github.com>
2020-05-13 11:17:38 +0800

    Update docker_run.sh

--- Sink/docker_run.sh
+++ Sink/docker_run.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-CONNECT_CONFIG_ID=ocid1.connectharness.oc1.uk-london-1.amaaaaaawiclygaagarqnaynpvnqgxiebsqo4x3h3hq4amsesmr7h2t3fqpa
+CONNECT_CONFIG_ID=ocid1.connectharness.oc1.ReplaceWithYours
 
 CONFIG_STORAGE_TOPIC=$CONNECT_CONFIG_ID-config
 OFFSET_STORAGE_TOPIC=$CONNECT_CONFIG_ID-offset
@@ -9,7 +9,7 @@ docker build -t kafkasinkconnect .
 
 docker run -it --rm --name sinktest -p 8084:8083 \
 -e GROUP_ID=sink-demo-group \
--e BOOTSTRAP_SERVERS=cell-1.streaming.uk-london-1.oci.oraclecloud.com:9092 \
+-e BOOTSTRAP_SERVERS=cell-1.streaming.YourOCIRegion.oci.oraclecloud.com:9092 \
 -e CONFIG_STORAGE_TOPIC=$CONFIG_STORAGE_TOPIC \
 -e OFFSET_STORAGE_TOPIC=$OFFSET_STORAGE_TOPIC \
 -e STATUS_STORAGE_TOPIC=$STATUS_STORAGE_TOPIC \

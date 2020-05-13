b6220b210b95ee9072850be9aafc1c57f2ea6e36
jhan <35212672+orajhan@users.noreply.github.com>
2020-05-13 11:16:33 +0800

    Update docker_run.sh

--- Source/docker_run.sh
+++ Source/docker_run.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-CONNECT_CONFIG_ID=ocid1.connectharness.oc1.uk-london-1.amaaaaaawiclygaagarqnaynpvnqgxiebsqo4x3h3hq4amsesmr7h2t3fqpa
+CONNECT_CONFIG_ID=ocid1.connectharness.oc1.ReplaceWithYours
 
 CONFIG_STORAGE_TOPIC=$CONNECT_CONFIG_ID-config
 OFFSET_STORAGE_TOPIC=$CONNECT_CONFIG_ID-offset
@@ -8,8 +8,8 @@ STATUS_STORAGE_TOPIC=$CONNECT_CONFIG_ID-status
 docker build -t kafkasourceconnecttest .
 
 docker run -it --rm --name sourcetest -p 8083:8083 \
--e GROUP_ID=demo-group \
--e BOOTSTRAP_SERVERS=cell-1.streaming.uk-london-1.oci.oraclecloud.com:9092 \
+-e GROUP_ID=source-demo-group \
+-e BOOTSTRAP_SERVERS=cell-1.streaming.YourRegion.oci.oraclecloud.com:9092 \
 -e CONFIG_STORAGE_TOPIC=$CONFIG_STORAGE_TOPIC \
 -e OFFSET_STORAGE_TOPIC=$OFFSET_STORAGE_TOPIC \
 -e STATUS_STORAGE_TOPIC=$STATUS_STORAGE_TOPIC \
